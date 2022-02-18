356c9d47ab015c59a4b87764bfda4c7c94f3edee
Padraig O'Donoghue <padraigo.donoghue@gmail.com>
2022-02-18 14:30:08 +0000

    Added icons with styles

--- src/ui-components/Content.js
+++ src/ui-components/Content.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import css from "./content.module.css"
+import Toolbar from './Toolbar'
 
 function Content() {
+    const actionMenu = [
+        {name: "add", onClick: () => alert("Add") },
+        {name: "delete", onClick: () => alert("Delete") }
+    ]
+
+    const zoomMenu = [
+        {name: "zoomIn", onClick: () => alert("Zoom In") },
+        {name: "zoomOut", onClick: () => alert("Zoom Out") },
+        {name: "panTool", onClick: () => alert("Pan Tool") }
+    ]
+
+    const viewMenu = [
+        {name: "viewList", onClick: () => alert("View List") },
+        {name: "hive", onClick: () => alert("View Chart") },
+    ]
   return (
-    <div className={css.container}>Content</div>
+    <div className={css.container}>
+        <Toolbar list={actionMenu} type="alert" location={["vertical", "right", "bottom"]} />
+        <Toolbar list={zoomMenu} type="default" location={["horizontal", "right", "top"]} />
+        <Toolbar list={viewMenu} type="default" location={["vertical", "left", "bottom"]} />
+    </div>
   )
 }
 
--- src/ui-components/LeftMenu.js
+++ src/ui-components/LeftMenu.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import css from "./leftmenu.module.css"
+import Toolbar from './Toolbar'
 
 function LeftMenu() {
+  const list = [
+    {name: "home", onClick: () => alert("Home") },
+    {name: "help", onClick: () => alert("Help") }
+]
   return (
-    <div className={css.container}>Left Menu</div>
+    <div className={css.container}>
+      <Toolbar list={list} type="primary" location={["vertical"]} />
+    </div>
   )
 }
 
